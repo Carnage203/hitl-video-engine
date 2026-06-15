b3860d0abe48c9e9b30a10ef2c730c3d7dbc909f
Carnage203 <89087056+Carnage203@users.noreply.github.com>
2026-06-15 14:08:34 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,11 @@
 from sqlmodel import SQLModel, create_engine, Session
 from config import SUPABASE_PROJECT_URL
 
-engine = create_engine(SUPABASE_PROJECT_URL, echo=False, connect_args={"check_same_thread": False})
+engine = create_engine(SUPABASE_PROJECT_URL, echo=False)
 
+def create_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
